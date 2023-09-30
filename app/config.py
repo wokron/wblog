a24878606a4509ae3e47ec4ac672afa3a925331d
wokron <stringcatwok@gmail.com>
2023-09-30 19:20:34 +0800

    feat: database connect config

--- app/config.py
+++ app/config.py
@@ -2,11 +2,27 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
 
+class DBQuerySettings(BaseSettings):
+    model_config = SettingsConfigDict(extra="allow")
+
+
 class Settings(BaseSettings):
     secret_key: str = Field(min_length=1)
     algorithm: str = "HS256"
     access_token_expire_minutes: int = Field(30, ge=1)
-    owner_name: str = Field("Owner", min_length=1, max_length=20)
-    owner_password: str = Field("12345678", min_length=8, max_length=24, pattern="^[0-9a-zA-Z]+$")
 
-    model_config = SettingsConfigDict(env_file=".env")
+    owner_name: str = Field("Owner", min_length=1, max_length=20)
+    owner_password: str = Field(
+        "12345678", min_length=8, max_length=24, pattern="^[0-9a-zA-Z]+$"
+    )
+
+    db_url: str | None = None
+    db_drivername: str = "sqlite"
+    db_username: str | None = Field(None, min_length=1)
+    db_password: str | None = Field(None, min_length=1)
+    db_host: str | None = Field(None, min_length=1)
+    db_port: int | None = Field(None, ge=0, le=65535)
+    db_database: str = Field("wblog.db", min_length=1)
+    db_query: DBQuerySettings = DBQuerySettings(check_same_thread="true")
+
+    model_config = SettingsConfigDict(env_file=".env", env_nested_delimiter="__")
--- app/core/database.py
+++ app/core/database.py
@@ -1,11 +1,25 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker, declarative_base
+from ..dependencies import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./dev.db" # sqlite only for dev
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # sqlite only
-)
+settings = config.get_settings()
+
+if settings.db_url is not None:
+    connect_url = settings.db_url
+else:
+    connect_url = URL.create(
+        drivername=settings.db_drivername,
+        username=settings.db_username,
+        password=settings.db_password,
+        host=settings.db_host,
+        port=settings.db_port,
+        database=settings.db_database,
+        query=settings.db_query.model_dump(),
+    )
+
+engine = create_engine(connect_url)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
