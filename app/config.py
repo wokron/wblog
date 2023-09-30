423a3686672fc1040078042cc0671bb3583905e2
wokron <stringcatwok@gmail.com>
2023-09-30 17:52:30 +0800

    feat: env file and validator in Settings

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 
 class Settings(BaseSettings):
-    secret_key: str
+    secret_key: str = Field(min_length=1)
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
-    owner_name: str = "Owner"
-    owner_password: str = "123456"
+    access_token_expire_minutes: int = Field(30, ge=1)
+    owner_name: str = Field("Owner", min_length=1, max_length=20)
+    owner_password: str = Field("12345678", min_length=8, max_length=24, pattern="^[0-9a-zA-Z]+$")
+
+    model_config = SettingsConfigDict(env_file=".env")
