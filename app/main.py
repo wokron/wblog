3f8a0bf4d705770d0c9a3c5e0e01ede89e19c3b1
wokron <stringcatwok@gmail.com>
2023-10-03 22:21:01 +0800

    feat: handle CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import router
 from . import database
@@ -6,5 +7,17 @@ from . import database
 database.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
